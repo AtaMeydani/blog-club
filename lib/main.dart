7f567241c56a242866ec3d267244c8c306df270a
Ata Meydani <miratameydani@gmail.com>
2023-04-01 19:29:46 +0330

    Implement ArticleScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:blog_club/article_screen.dart';
 import 'package:blog_club/auth_screen.dart';
 import 'package:blog_club/gen/assets.gen.dart';
 import 'package:blog_club/gen/fonts.gen.dart';
@@ -40,70 +41,79 @@ class MainApp extends StatelessWidget {
       //   ],
       // ),
       // home: const OnboardingScreen(),
-      home: const AuthScreen(),
+      // home: const AuthScreen(),
+      home: const ArticleScreen(),
       theme: ThemeData(
-        colorScheme: const ColorScheme.light(
-          primary: primaryColor,
-          onPrimary: Colors.white,
-          onSurface: primaryTextColor,
-          onBackground: primaryTextColor,
-          background: Color(0xFFFBFBFF),
-          surface: Colors.white,
-        ),
-        textButtonTheme: const TextButtonThemeData(
-          style: ButtonStyle(
-            textStyle: MaterialStatePropertyAll(
-              TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
+          colorScheme: const ColorScheme.light(
+            primary: primaryColor,
+            onPrimary: Colors.white,
+            onSurface: primaryTextColor,
+            onBackground: primaryTextColor,
+            background: Color(0xFFFBFBFF),
+            surface: Colors.white,
+          ),
+          textButtonTheme: const TextButtonThemeData(
+            style: ButtonStyle(
+              textStyle: MaterialStatePropertyAll(
+                TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                ),
               ),
             ),
           ),
-        ),
-        textTheme: const TextTheme(
-          titleSmall: TextStyle(
-            fontFamily: FontFamily.avenir,
-            color: primaryTextColor,
-            fontSize: 14,
-            fontWeight: FontWeight.w400,
+          textTheme: const TextTheme(
+            titleSmall: TextStyle(
+              fontFamily: FontFamily.avenir,
+              color: primaryTextColor,
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+            titleMedium: TextStyle(
+              fontFamily: FontFamily.avenir,
+              color: secondaryTextColor,
+              fontSize: 18,
+              fontWeight: FontWeight.w200,
+            ),
+            titleLarge: TextStyle(
+              fontFamily: FontFamily.avenir,
+              fontWeight: FontWeight.bold,
+              color: primaryTextColor,
+              fontSize: 18,
+            ),
+            headlineMedium: TextStyle(
+              fontFamily: FontFamily.avenir,
+              fontWeight: FontWeight.w700,
+              fontSize: 24,
+              color: primaryTextColor,
+            ),
+            headlineSmall: TextStyle(
+              fontFamily: FontFamily.avenir,
+              fontSize: 20,
+              color: primaryTextColor,
+              fontWeight: FontWeight.w700,
+            ),
+            bodyMedium: TextStyle(
+              fontFamily: FontFamily.avenir,
+              color: secondaryTextColor,
+              fontSize: 12,
+            ),
+            bodySmall: TextStyle(
+              fontFamily: FontFamily.avenir,
+              fontWeight: FontWeight.w700,
+              fontSize: 10,
+              color: Color(0xff7B8BB2),
+            ),
           ),
-          titleMedium: TextStyle(
-            fontFamily: FontFamily.avenir,
-            color: secondaryTextColor,
-            fontSize: 18,
-            fontWeight: FontWeight.w200,
+          appBarTheme: const AppBarTheme(
+            titleSpacing: 32,
+            backgroundColor: Colors.white,
+            foregroundColor: primaryColor,
+            elevation: 0,
           ),
-          titleLarge: TextStyle(
-            fontFamily: FontFamily.avenir,
-            fontWeight: FontWeight.bold,
-            color: primaryTextColor,
-            fontSize: 18,
-          ),
-          headlineMedium: TextStyle(
-            fontFamily: FontFamily.avenir,
-            fontWeight: FontWeight.w700,
-            fontSize: 24,
-            color: primaryTextColor,
-          ),
-          headlineSmall: TextStyle(
-            fontFamily: FontFamily.avenir,
-            fontSize: 20,
-            color: primaryTextColor,
-            fontWeight: FontWeight.w700,
-          ),
-          bodyMedium: TextStyle(
-            fontFamily: FontFamily.avenir,
-            color: secondaryTextColor,
-            fontSize: 12,
-          ),
-          bodySmall: TextStyle(
-            fontFamily: FontFamily.avenir,
-            fontWeight: FontWeight.w700,
-            fontSize: 10,
-            color: Color(0xff7B8BB2),
-          ),
-        ),
-      ),
+          snackBarTheme: const SnackBarThemeData(
+            backgroundColor: primaryColor,
+          )),
     );
   }
 }
